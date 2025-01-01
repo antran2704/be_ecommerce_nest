6ab8912c3e2ccc2230b9d4e11e89fdd74765effd
an tran <antran.teachhert@gmail.com>
2025-01-01 17:32:46 +0700

    feat<Global>: create Pine and ExceptionFilter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from '../common/common.module';
--- src/app/middlewares/middleware.module.ts
+++ src/app/middlewares/middleware.module.ts
@@ -1,7 +1,25 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  ValidationPipe,
+} from '@nestjs/common';
 import { HelmetMiddleware } from './helmet.middleware';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { HttpExceptionFilter } from 'src/common/exceptionFilter/http-exception.filter';
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
+})
 export class MiddlewareModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(HelmetMiddleware).forRoutes('*');
