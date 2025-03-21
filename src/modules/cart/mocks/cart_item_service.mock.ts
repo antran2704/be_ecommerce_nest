9fd1fc370855cc27eef998333dab7892cef57339
An Tran <it.dev.fe@tinigroup.vn>
2025-03-21 17:11:20 +0700

    test(CartItem): add test case for module

---
+++ src/modules/cart/mocks/cart_item_service.mock.ts
@@ -0,0 +1,59 @@
+import { ENUM_PAGINATION_ORDER } from "~/common/pagination/enums/order.enum";
+import { ENUM_PREFIX_DATABASE } from "~/common/database/enums/perfix.enum";
+import { IEntitesAndPaginationReponse } from "~/common/pagination/interfaces/pagination.interface";
+import {
+  CreateCartItemRequestDto,
+  GetCartItemResponseDto,
+  GetCartItemsRequestDto,
+} from "../dtos/services";
+
+const mockCreateCartItemRequest: CreateCartItemRequestDto = {
+  cartId: ENUM_PREFIX_DATABASE.CAR + "123",
+  productId: ENUM_PREFIX_DATABASE.PR + "123",
+  variantProductId: ENUM_PREFIX_DATABASE.VPR + "123",
+  quantity: 1,
+};
+
+const mockGetCartItemsRequest: GetCartItemsRequestDto = {
+  limit: 10,
+  order: ENUM_PAGINATION_ORDER.DESC,
+  page: 1,
+  cardId: ENUM_PREFIX_DATABASE.CAR + "123",
+};
+
+const mockGetCartItemResponse: Partial<GetCartItemResponseDto> = {
+  cartItemId: ENUM_PREFIX_DATABASE.CARI + "123",
+  inventory: 10,
+  price: 100,
+  quantity: 1,
+  promotionPrice: 10,
+  thumbnail: "thumbnail",
+  product: {
+    name: "product name",
+    productId: ENUM_PREFIX_DATABASE.PR + "123",
+  },
+  variantProduct: {
+    name: "variant product name",
+    variantProductId: ENUM_PREFIX_DATABASE.VPR + "123",
+    options: [],
+  },
+};
+
+const mockGetCartItemsResponse: IEntitesAndPaginationReponse<
+  Partial<GetCartItemResponseDto>
+> = {
+  data: [mockGetCartItemResponse],
+  pagination: {
+    take: 10,
+    page: 1,
+    total: 10,
+    totalPages: 1,
+  },
+};
+
+export {
+  mockCreateCartItemRequest,
+  mockGetCartItemsRequest,
+  mockGetCartItemsResponse,
+  mockGetCartItemResponse,
+};
